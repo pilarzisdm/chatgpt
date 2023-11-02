68ef87b4d86221a3e2b9b88514964ee4a4965bc0
pilarzisdm <149374220+pilarzisdm@users.noreply.github.com>
2023-11-02 08:29:47 +0000

    y

--- market_price_prediction.py
+++ market_price_prediction.py
@@ -16,12 +16,12 @@ data = load_data()
 st.title('Web Peramalan Harga Komoditas')
 
 # Sidebar
-st.sidebar.header('Data Settings')
-features = st.sidebar.multiselect('Select Features', data.columns)
+#st.sidebar.header('Data Settings')
+features = st.sidebar.multiselect('Pilih Data', data.columns)
 
 # Choose features (independent variables) and the target (dependent variable)
 if not features:
-    st.warning('Please select at least one feature.')
+    st.warning('Silakan pilih minimal satu komoditas.')
 else:
     target = st.sidebar.selectbox('Select Target', data.columns)
 
@@ -43,16 +43,16 @@ else:
     mse = mean_squared_error(y_test, predictions)
     r2 = r2_score(y_test, predictions)
 
-    st.subheader('Model Evaluation')
+    st.subheader('Evaluasi Model')
     st.write(f"Mean Squared Error: {mse}")
     st.write(f"R-squared: {r2}")
 
     # Plot the actual vs. predicted prices
     fig, ax = plt.subplots()
     ax.scatter(y_test, predictions)
-    ax.set_xlabel("Actual Price")
-    ax.set_ylabel("Predicted Price")
-    ax.set_title("Actual vs. Predicted Prices")
+    ax.set_xlabel("Harga Aktual")
+    ax.set_ylabel("Harga Prediksi")
+    ax.set_title("Harga Aktual vs Harga Prediksi")
 
     # Save the plot to a BytesIO object
     img_buf = io.BytesIO()
@@ -60,17 +60,17 @@ else:
     st.image(img_buf)
 
     # Market Price Forecast
-    st.subheader('Market Price Forecast')
-    user_input = st.text_input('Enter feature values separated by commas')
+    st.subheader('Peramalan Harga Komoditas')
+    user_input = st.text_input('Silakan masukan nilai')
 
     if user_input:
         try:
             user_input = [float(x.strip()) for x in user_input.split(',')]  # Convert to a list of floats
             if len(user_input) == len(features):
                 forecast = model.predict([user_input])
-                st.write(f"Forecasted Price: {forecast[0]}")
+                st.write(f"Harga Prediksi: {forecast[0]}")
             else:
                 st.warning(f"Please provide {len(features)} feature values.")
         except ValueError:
-            st.warning("Invalid input. Please enter numeric values separated by commas.")
+            st.warning("Format input salah. Silakan memasukan format numerik.")
 
