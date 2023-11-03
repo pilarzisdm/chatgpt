d8cc7c2b33ead2dcb3bebd859cebb19aef7acae7
pilarzisdm <149374220+pilarzisdm@users.noreply.github.com>
2023-11-03 08:05:16 +0000

    y

--- market_price_prediction.py
+++ market_price_prediction.py
@@ -1,76 +1,46 @@
 import streamlit as st
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
-import io
 
-# Load your dataset (change 'your_dataset.csv' to your file's name)
-@st.cache_data
+# Load the CSV data
+@st.cache
 def load_data():
-    data = pd.read_csv('harga.csv')
+    data = pd.read_csv("harga.csv")
     return data
 
+# Sidebar: Select commodities
+st.sidebar.title("Select Commodities")
+commodities = st.sidebar.multiselect("Select one or more commodities", ["Beras", "Daging Ayam", "Telur Ayam", "Cabai Merah", "Cabai Rawit"])
+
+# Main content
+st.title("Daily Commodity Price Forecasting")
+
+# Load data
 data = load_data()
 
-st.title('Web Peramalan Harga Komoditas')
+if len(commodities) > 0:
+    # Filter data based on selected commodities
+    selected_data = data[['date'] + commodities]
+    st.subheader("Commodity Prices")
+    st.write(selected_data.set_index('date'))
 
-# Sidebar
-#st.sidebar.header('Data Settings')
-features = st.sidebar.multiselect('Pilih Data', data.columns)
+    # Plot selected commodities
+    st.subheader("Commodity Price Chart")
+    for commodity in commodities:
+        plt.figure(figsize=(10, 5))
+        plt.plot(selected_data['date'], selected_data[commodity])
+        plt.xlabel("Date")
+        plt.ylabel("Price")
+        plt.title(f"{commodity} Price Over Time")
+        st.pyplot()
 
-# Choose features (independent variables) and the target (dependent variable)
-if not features:
-    st.warning('Silakan pilih minimal satu komoditas.')
+    # Forecasting period
+    st.subheader("Commodity Price Forecasting")
+    forecasting_period = st.number_input("Enter the forecasting period (in days):", min_value=1, step=1)
+    if st.button("Forecast"):
+        # Perform your forecasting calculations here using the selected commodities and the forecasting period
+        st.write(f"Forecasting for the next {forecasting_period} days for selected commodities")
+
 else:
-    target = st.sidebar.selectbox('Select Target', data.columns)
-
-    # Split the data into training and testing sets
-    X = data[features]
-    y = data[target]
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    # Create and train the model
-    model = LinearRegression()
-    model.fit(X_train, y_train)
-
-    # Make predictions
-    predictions = model.predict(X_test)
-
-    # Evaluate the model (you may want to use different metrics)
-    from sklearn.metrics import mean_squared_error, r2_score
-    mse = mean_squared_error(y_test, predictions)
-    r2 = r2_score(y_test, predictions)
-
-    st.subheader('Evaluasi Model')
-    st.write(f"Mean Squared Error: {mse}")
-    st.write(f"R-squared: {r2}")
-
-    # Plot the actual vs. predicted prices
-    fig, ax = plt.subplots()
-    ax.scatter(y_test, predictions)
-    ax.set_xlabel("Harga Aktual")
-    ax.set_ylabel("Harga Prediksi")
-    ax.set_title("Harga Aktual vs Harga Prediksi")
-
-    # Save the plot to a BytesIO object
-    img_buf = io.BytesIO()
-    plt.savefig(img_buf, format='png')
-    st.image(img_buf)
-
-    # Market Price Forecast
-    st.subheader('Peramalan Harga Komoditas')
-    user_input = st.text_input('Silakan masukan nilai')
-
-    if user_input:
-        try:
-            user_input = [float(x.strip()) for x in user_input.split(',')]  # Convert to a list of floats
-            if len(user_input) == len(features):
-                forecast = model.predict([user_input])
-                st.write(f"Harga Prediksi: {forecast[0]}")
-            else:
-                st.warning(f"Please provide {len(features)} feature values.")
-        except ValueError:
-            st.warning("Format input salah. Silakan memasukan format numerik.")
+    st.warning("Please select one or more commodities from the sidebar.")
 
