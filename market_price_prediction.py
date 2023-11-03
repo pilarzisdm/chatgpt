05b3e20e36137a7a3bad9283af7d107ff74a1eba
pilarzisdm <149374220+pilarzisdm@users.noreply.github.com>
2023-11-03 09:08:32 +0000

    y

--- market_price_prediction.py
+++ market_price_prediction.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 @st.cache_data
 def load_data():
     data = pd.read_csv("harga_real.csv")
-    #data['Tanggal'] = pd.to_datetime(data['Tanggal'])  # Parse the date column as datetime
+    data['Tanggal'] = pd.to_datetime(data['Tanggal'])  # Parse the date column as datetime
     return data
 
 # Sidebar: Select commodities
@@ -19,18 +19,21 @@ st.title("Peramalan Harga Komoditas Harian")
 # Load data
 data = load_data()
 
+# Filter data based on selected commodities
 if len(commodities) > 0:
-    # Filter data based on selected commodities
     selected_data = data[['Tanggal'] + commodities]
     selected_data = selected_data.sort_values(by='Tanggal', ascending=False)
-    
+
     st.subheader("Harga Komoditas")
     st.write(selected_data.set_index('Tanggal'))
+
+    # Add select box for granularity
+    granularity = st.selectbox("Pilih Granularitas Tanggal", ["Harian", "Mingguan", "Bulanan", "Tahunan"])
 
     # Plot selected commodities
     st.subheader("Grafik Harga Komoditas")
     fig, ax = plt.subplots(figsize=(10, 5))
-    
+
     for commodity in commodities:
         ax.plot(selected_data['Tanggal'], selected_data[commodity], label=commodity)
 
@@ -38,15 +41,35 @@ if len(commodities) > 0:
     ax.set_ylabel("Harga")
     ax.set_title("Harga Komoditas Antar Waktu")
     ax.legend()
-    
-    # Add slider for x-axis limits using the DataFrame index
-    x_min_idx, x_max_idx = st.slider("Pilih Rentang Tanggal", 0, len(selected_data) - 1, (0, len(selected_data) - 1))
-    x_min = selected_data['Tanggal'].iloc[x_min_idx]
-    x_max = selected_data['Tanggal'].iloc[x_max_idx]
+
+    # Determine granularity multiplier
+    if granularity == "Harian":
+        granularity_multiplier = 1
+    elif granularity == "Mingguan":
+        granularity_multiplier = 7
+    elif granularity == "Bulanan":
+        granularity_multiplier = 30  # Approximate number of days in a month
+    else:
+        granularity_multiplier = 365  # Approximate number of days in a year
+
+    # Calculate slider limits
+    min_date = selected_data['Tanggal'].min()
+    max_date = selected_data['Tanggal'].max()
+    x_min = st.date_input("Pilih Rentang Tanggal Awal", min_date, min_date, max_date - pd.DateOffset(days=1))
+    x_max = st.date_input("Pilih Rentang Tanggal Akhir", min_date + pd.DateOffset(days=granularity_multiplier), min_date, max_date)
+
+    # Filter data based on the selected date range
+    filtered_data = selected_data[(selected_data['Tanggal'] >= x_min) & (selected_data['Tanggal'] <= x_max)]
+
+    # Update the plot with the filtered data
+    for commodity in commodities:
+        ax.plot(filtered_data['Tanggal'], filtered_data[commodity], label=commodity)
+
+    # Set x-axis limits
     ax.set_xlim(x_min, x_max)
-    
+
     st.pyplot(fig)
-    
+
     # Forecasting period
     st.subheader("Peramalan Harga Komoditas")
     forecasting_period = st.number_input("Masukan periode peramalan (dalam hari):", min_value=1, step=1)
