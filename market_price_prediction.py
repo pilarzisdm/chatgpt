7c6f772c678521a57b9d55b59ee1084cccd7bd4b
pilarzisdm <149374220+pilarzisdm@users.noreply.github.com>
2023-11-03 09:13:38 +0000

    y

--- market_price_prediction.py
+++ market_price_prediction.py
@@ -3,9 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load the CSV data
-@st.cache_data
+@st.cache
 def load_data():
-    data = pd.read_csv("harga_real.csv")
+    data = pd.read_csv("harga.csv")
     data['Tanggal'] = pd.to_datetime(data['Tanggal'])  # Parse the date column as datetime
     return data
 
@@ -30,18 +30,6 @@ if len(commodities) > 0:
     # Add select box for granularity
     granularity = st.selectbox("Pilih Granularitas Tanggal", ["Harian", "Mingguan", "Bulanan", "Tahunan"])
 
-    # Plot selected commodities
-    st.subheader("Grafik Harga Komoditas")
-    fig, ax = plt.subplots(figsize=(10, 5))
-
-    for commodity in commodities:
-        ax.plot(selected_data['Tanggal'], selected_data[commodity], label=commodity)
-
-    ax.set_xlabel("Tanggal")
-    ax.set_ylabel("Harga")
-    ax.set_title("Harga Komoditas Antar Waktu")
-    ax.legend()
-
     # Determine granularity multiplier
     if granularity == "Harian":
         granularity_multiplier = 1
@@ -52,21 +40,18 @@ if len(commodities) > 0:
     else:
         granularity_multiplier = 365  # Approximate number of days in a year
 
-    # Calculate slider limits
-    min_date = selected_data['Tanggal'].min()
-    max_date = selected_data['Tanggal'].max()
-    x_min = st.date_input("Pilih Rentang Tanggal Awal", min_date, min_date, max_date - pd.DateOffset(days=1))
-    x_max = st.date_input("Pilih Rentang Tanggal Akhir", min_date + pd.DateOffset(days=granularity_multiplier), min_date, max_date)
+    # Plot selected commodities with the selected granularity
+    st.subheader("Grafik Harga Komoditas")
+    fig, ax = plt.subplots(figsize=(10, 5))
 
-    # Filter data based on the selected date range
-    filtered_data = selected_data[(selected_data['Tanggal'] >= x_min) & (selected_data['Tanggal'] <= x_max)]
-
-    # Update the plot with the filtered data
     for commodity in commodities:
+        filtered_data = selected_data[selected_data['Tanggal'] >= (selected_data['Tanggal'].max() - pd.DateOffset(days=granularity_multiplier))]
         ax.plot(filtered_data['Tanggal'], filtered_data[commodity], label=commodity)
 
-    # Set x-axis limits
-    ax.set_xlim(x_min, x_max)
+    ax.set_xlabel("Tanggal")
+    ax.set_ylabel("Harga")
+    ax.set_title("Harga Komoditas Antar Waktu")
+    ax.legend()
 
     st.pyplot(fig)
 
